c84bb7554e088f1d1a4158ffeb338c3d11369df3
caleb <caleb@syndicate.io>
2026-01-05 18:53:18 -0600

    up

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 				width={800}
 				height={1035}
 				priority
-				className="max-h-[80vh] w-auto animate-fade-in"
+				className="max-h-[100vh] w-auto animate-fade-in"
 			/>
 		</div>
 	);
